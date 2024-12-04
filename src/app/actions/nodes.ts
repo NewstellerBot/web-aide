0b9b2c063234309888ccb62178bf448d221c7898
Krystian Nowak <kr21032002@icloud.com>
2024-12-04 10:27:49 -0800

    nodes save to db

--- src/app/actions/nodes.ts
+++ src/app/actions/nodes.ts
@@ -7,27 +7,23 @@ export async function updateNodes(nodes: AideNode[]) {
     if (!process.env.POSTGRES_URL) throw new Error("No postgres url provided!")
     if (!nodes || nodes.length === 0) throw new Error("No nodes to update!")
 
-    const sql = neon(process.env.POSTGRES_URL)
-    const values = nodes
-        .map((n) => `(${n.id}, '${JSON.stringify(n)}')`).join(',\n')
+    try {
+        const sql = neon(process.env.POSTGRES_URL)
+        const savePromises = nodes.map(n => {
+            return sql`
+        INSERT INTO nodes (id, data)
+        VALUES
+        (${n.id}, ${JSON.stringify(n)})
+        ON CONFLICT (id)
+        DO UPDATE SET
+            data = EXCLUDED.data;
+        `;
+        })
 
-    console.log('executing sql: ', `
-      INSERT INTO nodes (id, data)
-      VALUES
-      ${values}
-      ON CONFLICT (id) DO UPDATE
-      SET data = EXCLUDED.data;
-    `)
-
-    // await sql`
-    //   INSERT INTO nodes (id, data)
-    //   VALUES
-    //   ${values}
-    //   ON CONFLICT (id) DO UPDATE
-    //   SET data = EXCLUDED.data;
-    // `;
-
-    // console.log(values)
-    // console.log(nodes)
+        return await Promise.allSettled(savePromises)
+    } catch (e) {
+        // TODO: add better error handling
+        console.error(e);
+    }
 }
 
