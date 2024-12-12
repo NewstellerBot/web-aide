4db74eabc5c45a64c55f38695422a0766706296d
Krystian Nowak <kr21032002@icloud.com>
2024-12-11 22:13:43 -0800

    bug fix: id length update

--- src/lib/sql/create_tables.sql
+++ src/lib/sql/create_tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS workflows (
-    id VARCHAR(36) DEFAULT gen_random_uuid() PRIMARY KEY,
+    id VARCHAR(128) DEFAULT gen_random_uuid() PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     name VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS nodes (
     id VARCHAR(128) DEFAULT gen_random_uuid() PRIMARY KEY,
     data JSONB,
-    workflow_id VARCHAR(36),
+    workflow_id VARCHAR(128),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_workflow_nodes FOREIGN KEY(workflow_id) REFERENCES workflows(id) ON DELETE CASCADE
 );
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS edges (
     id VARCHAR(128) DEFAULT gen_random_uuid() PRIMARY KEY,
     source VARCHAR(128),
     target VARCHAR(128),
-    workflow_id VARCHAR(36),
+    workflow_id VARCHAR(128),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_workflow_edges FOREIGN KEY(workflow_id) REFERENCES workflows(id) ON DELETE CASCADE
 );
