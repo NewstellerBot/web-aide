5e96ec23ea4e97c799922d4f871320654ff0a846
Krystian Nowak <kr21032002@icloud.com>
2025-01-22 14:28:59 -0800

    dev: log body for debug

--- src/app/api/telegram/webhook/route.ts
+++ src/app/api/telegram/webhook/route.ts
@@ -1,13 +1,17 @@
+// import TelegramBot from "node-telegram-bot-api";
+
 import { updateSchema } from "@/app/api/telegram/schema";
+
+// const TG_TOKEN = "8011966300:AAEXtz289cMCsuqVT_Zm5DVqRRix6VHCfOI";
 
 export async function POST(req: Request) {
   const body = (await req.json()) as unknown;
+  console.log(body);
   const parseResult = updateSchema.safeParse(body);
   if (!parseResult.success) {
     return Response.json({ error: parseResult.error.message }, { status: 400 });
   }
   const tg = parseResult.data;
-  console.log(tg);
 
   return Response.json({ success: true });
 }
