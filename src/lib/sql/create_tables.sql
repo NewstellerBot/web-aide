ad9e3db7463d83ac0e13e4384200849afa8873af
Krystian Nowak <kr21032002@icloud.com>
2025-01-25 23:41:21 -0800

    bug  fix: add user_id and timestamp to schema

--- src/lib/sql/create_tables.sql
+++ src/lib/sql/create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS workflows (
     id VARCHAR(128) DEFAULT gen_random_uuid() PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id VARCHAR(128),
     name VARCHAR(255)
 );
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS nodes (
     id VARCHAR(128) DEFAULT gen_random_uuid() PRIMARY KEY,
     data JSONB,
     workflow_id VARCHAR(128),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id VARCHAR(128),
     CONSTRAINT fk_workflow_nodes FOREIGN KEY(workflow_id) REFERENCES workflows(id) ON DELETE CASCADE
 );
@@ -17,16 +17,19 @@ CREATE TABLE IF NOT EXISTS edges (
     source VARCHAR(128),
     target VARCHAR(128),
     workflow_id VARCHAR(128),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id VARCHAR(128),
     CONSTRAINT fk_workflow_edges FOREIGN KEY(workflow_id) REFERENCES workflows(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS knowledge (
     id VARCHAR(128) DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(256)
+    name VARCHAR(256),
+    user_id VARCHAR(128),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE EXTENSION vector;
+CREATE EXTENSION IF NOT EXISTS vector;
 CREATE TABLE items (
     id BIGSERIAL PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     embedding VECTOR(1536)
 );
