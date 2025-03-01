46ddc38c6325d692984844b763419cde01d03dc6
Krystian Nowak <kr21032002@icloud.com>
2025-02-28 18:42:26 -0800

    feat: jwt decode and verify

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -9,6 +9,24 @@ const base64urlEncode = (str: string): string => {
     .replace(/=/g, "");
 };
 
+const base64urlDecode = (str: string): string => {
+  // Add padding back
+  str = str.replace(/-/g, "+").replace(/_/g, "/");
+  switch (str.length % 4) {
+    case 0:
+      break;
+    case 2:
+      str += "==";
+      break;
+    case 3:
+      str += "=";
+      break;
+    default:
+      throw new Error("Invalid base64url string");
+  }
+  return Buffer.from(str, "base64").toString();
+};
+
 export const signJWT = (payload: object) => {
   const header = {
     alg: "HS256",
@@ -25,11 +43,24 @@ export const signJWT = (payload: object) => {
   return stringifiedHeader + "." + stringifiedPayload + "." + signature;
 };
 
-export const verifyJWT = (token: string) => {
+export const verifyJWT = (
+  token: string,
+): { success: boolean; payload: unknown } => {
   const [header, payload, signature] = token.split(".");
+  if (!header || !payload || !signature) {
+    return {
+      success: false,
+      payload: null,
+    };
+  }
+
   const signatureToVerify = crypto
     .createHmac("sha256", env.TELEGRAM_SECRET)
     .update(header + "." + payload)
     .digest("base64");
-  return signatureToVerify === signature;
+  const success = signatureToVerify === signature;
+  return {
+    success,
+    payload: success ? JSON.parse(base64urlDecode(payload)) : null,
+  };
 };
